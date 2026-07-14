bff56d94d22cec7f098099cc5468b5fabcc7f58b
taufiqarinta <taufiqardiyono33@gmail.com>
2026-07-14 11:31:05 +0700

    update hasil rapat dengan bu belinda

--- resources/views/masterdoorprize/index.blade.php
+++ resources/views/masterdoorprize/index.blade.php
@@ -34,7 +34,7 @@
             z-index: 5;
         }
         
-        .max-w-7xl {
+        .max-w-9xl {
             position: relative;
             z-index: 10;
         }
@@ -47,7 +47,7 @@
     </style>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-9xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     @if(session('success'))

d27196d05b4478acad5d1e90707836f1b3c668b7
taufiqarinta <taufiqardiyono33@gmail.com>
2026-09-02 15:14:57 +0700

    ganti nomoer mas reza

--- app/Http/Controllers/FormOrderController.php
+++ app/Http/Controllers/FormOrderController.php
@@ -1956,7 +1956,8 @@ class FormOrderController extends Controller
 
             // $this->sendWaDirect('6282301525560', $pesan); pak maulana
             // $this->sendWaDirect('6285648255747', $pesan); // mas fanani
-            $this->sendWaDirect('082131495585', $pesan); // mas Andrew
+            // $this->sendWaDirect('082131495585', $pesan); // mas taufiq
+            $this->sendWaDirect('082337685726', $pesan); // mas Andrew
         } catch (\Exception $e) {
             \Log::error('Gagal kirim notifikasi over limit: ' . $e->getMessage());
         }
